dc079acd064ee5bd2cf08ed8c86b37f08b255f44
Liga Auguste <ligaauguste@gmail.com>
2025-01-15 00:52:52 +0100

    task solved

---
+++ 05-if-and-switch-statements/script.js
@@ -0,0 +1,59 @@
+const size = 25;
+let result;
+
+if (size > 20) {
+  result = "Greater than 20";
+} else if (size > 10) {
+  result = "Greater than 10";
+} else {
+  result = "Lower than 10";
+}
+
+console.log(result);
+// Returns Greater than 10 but should return Greater than 20
+
+// TODO: Implement the oddEven function
+function oddEven(number) {
+  if (number % 2 === 0) {
+    return "even";
+  } else {
+    return "odd";
+  }
+}
+
+console.log(oddEven(4));
+// result should be even
+
+console.log(oddEven(3));
+// result should be odd
+
+console.log(oddEven(-1));
+// result should be odd
+
+console.log(oddEven(10));
+// result should be even
+
+// TODO: Implement the oldYoung function
+function oldYoung(age) {
+  if (isNaN(age) || age < 0) {
+    return "invalid parameter";
+  } else if (age < 16) {
+    return "child";
+  } else if (age <= 50) {
+    return "young person";
+  } else {
+    return "elder person";
+  }
+}
+
+console.log(oldYoung(27));
+// result should be young person
+
+console.log(oldYoung(6));
+// result should be children
+
+console.log(oldYoung(-1));
+// result should be invalid parameter
+
+console.log(oldYoung(86));
+// result should be elder person
